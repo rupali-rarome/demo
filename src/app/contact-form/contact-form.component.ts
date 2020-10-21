ca4d8a29908445a54e4189859579c0969fa9d126
rupalimarwadi <rupali0103@gmail.com>
2020-10-22 02:41:40 +0530

    Template driven form (validation,ngModel, NgFromGroup, NgForm, ngValue)

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -5,11 +5,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
-export class ContactFormComponent implements OnInit {
+export class ContactFormComponent {
+  contactMethod = [{ id: 1, name: 'Email' }, { id: 2, name: 'Phone' }, { id: 3, name: 'In-Personal' }];
+  gender = [{ id: 1, name: 'Male' }, { id: 2, name: 'Female' }];
 
-  constructor() { }
+  log(x) {
+    console.log(x);
+  }
 
-  ngOnInit(): void {
+  submit(f) {
+    console.log(f);
   }
 
 }
