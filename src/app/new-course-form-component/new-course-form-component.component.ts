f7b7ea56df64ac97e00a20982718a39c4a266a16
rupalimarwadi <rupali0103@gmail.com>
2020-10-23 15:49:52 +0530

    Eg of form builder

--- src/app/new-course-form-component/new-course-form-component.component.ts
+++ src/app/new-course-form-component/new-course-form-component.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'new-course-form-component',
@@ -7,7 +7,33 @@ import { FormArray, FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./new-course-form-component.component.css']
 })
 export class NewCourseFormComponentComponent {
-
+  // // Below are different approaches. For example purpose
+  // // Below Mentioned can be rewrite using formBuilder class.
+  // //Approach 1
+  // forms = new FormGroup({
+  //   name: new FormControl(),
+  //   contact: new FormGroup({
+  //     phone: new FormControl('', Validators.required),
+  //     email: new FormControl(),
+  //   }),
+  //   topics: new FormArray([]),
+  // });
+  // //End of Approach 1
+  // // rewritten using form builder class
+  // //Approach 2
+  // forms;
+  // constructor(fb: FormBuilder) {
+  //   this.forms = fb.group({
+  //     name: [],
+  //     contact: fb.group({
+  //       phone: [],
+  //       email: []
+  //     }),
+  //     topics: fb.array
+  //   })
+  // }
+  // //End of Approach 
+  
   form = new FormGroup({
     topics: new FormArray([]),
   });
